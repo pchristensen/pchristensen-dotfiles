18ffeb704bdcc2c591759d2a2426223bd7cfbaa1
Peter Christensen <peter@pchristensen.com>
2011-05-13 16:51:49 -0500

    Added vendor defun and tested it out with haml and sass mode; need to migrate other submodule libraries here next

--- emacs.d/pchristensen-emacs.el
+++ emacs.d/pchristensen-emacs.el
@@ -3,37 +3,38 @@
 (setq custom-file "~/.emacs.d/pchristensen/custom.el")
 (load custom-file)
 
-(require 'cl)
+(load "pchristensen/defuns")
 
-; from http://steve.yegge.googlepages.com/my-dot-emacs-file
-(defvar emacs-root (cond ((or (eq system-type 'gnu/linux) (eq system-type 'cyqwin) (eq system-type 'linux)) "/home/peter/")
-			 ((eq system-type 'darwin) "/Users/peterchristensen/")
-			 (t "C:/Users/Peter/Documents/nix/")))
+(vendor 'haml-mode)
+(vendor 'sass-mode)
+;-----------------------------------
 
-;;TODO Questionable definitions
-;(defvar elisp-root "src/elisp/")
-;(add-to-list 'load-path (concat emacs-root "src/lisp/"))
-;(add-to-list 'load-path (concat emacs-root "src/elisp/"))
+;; (require 'cl)
 
-(labels ((add-path (p)
-		   (add-to-list 'load-path (concat emacs-root p))))
-  (add-path "emacs/lisp") ;; all my personal elisp code
-  (add-path "emacs/site-lisp") ;; elisp stuff I find on the internet
-  (add-path "emacs/lisp/cnu") ;; Specific to CashNetUSA setup
-)
+;; ; from http://steve.yegge.googlepages.com/my-dot-emacs-file
+;; (defvar emacs-root (cond ((or (eq system-type 'gnu/linux) (eq system-type 'cyqwin) (eq system-type 'linux)) "/home/peter/")
+;; 			 ((eq system-type 'darwin) "/Users/peterchristensen/")
+;; 			 (t "C:/Users/Peter/Documents/nix/")))
 
-(load-library "display-prefs")  ;; Make the windows look the way I want
-(load-library "behavior") ;; One-offs and configs to make emacs act the way I want
-(load-library "utils") ;; Helpful functions and snippets I've collected
-(load-library "modes") ;; Setup and customization for different modes
+;; ;;TODO Questionable definitions
+;; ;(defvar elisp-root "src/elisp/")
+;; ;(add-to-list 'load-path (concat emacs-root "src/lisp/"))
+;; ;(add-to-list 'load-path (concat emacs-root "src/elisp/"))
 
-;;--------PROJECT----------------------------------------------------------------------------
-(load-library "groupon") ;; Stuff specific to Groupon
-(load-library "geekstack") ;; Stuff specific to GeekStack work
+;; (labels ((add-path (p)
+;; 		   (add-to-list 'load-path (concat emacs-root p))))
+;;   (add-path "emacs/lisp") ;; all my personal elisp code
+;;   (add-path "emacs/site-lisp") ;; elisp stuff I find on the internet
+;;   (add-path "emacs/lisp/cnu") ;; Specific to CashNetUSA setup
+;; )
 
-(load-library "custom") ;;layout customizations
+;; (load-library "display-prefs")  ;; Make the windows look the way I want
+;; (load-library "behavior") ;; One-offs and configs to make emacs act the way I want
+;; (load-library "utils") ;; Helpful functions and snippets I've collected
+;; (load-library "modes") ;; Setup and customization for different modes
 
-; Local Variables:
-; mode: emacs-lisp
-; End:
+;; ;;--------PROJECT----------------------------------------------------------------------------
+;; (load-library "groupon") ;; Stuff specific to Groupon
+;; (load-library "geekstack") ;; Stuff specific to GeekStack work
 
+;; (load-library "custom") ;;layout customizations
