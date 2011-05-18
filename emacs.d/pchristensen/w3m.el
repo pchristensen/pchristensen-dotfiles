0c0a1860e01469633173f34468f95791fda81b17
Peter Christensen <peter@pchristensen.com>
2011-05-17 22:32:45 -0500

    Disabled any reference to emacs-w3m

--- emacs.d/pchristensen/w3m.el
+++ emacs.d/pchristensen/w3m.el
@@ -1,12 +1,14 @@
+;; ;; TODO It's so doubtful if emacsw3m is worth it
+
 ;;---------W3M------------------------------------------------------------------------------------------
 ;;http://emacs-w3m.namazu.org/info/emacs-w3m_10.html#SEC10
 ;;http://w3m.sourceforge.net/MANUAL#Key:orig
 ;;http://bc.tech.coop/blog/070208.html - ;;Bill Clementson: SLIME Tips and Techniques - Part 5 (Alternative Browsers in Emacs)
-(if (or (eq system-type 'gnu/linux)
-	(eq system-type 'cyqwin)
-	(eq system-type 'linux))
-    (progn
-      (add-to-list 'load-path "/usr/share/emacs/site-lisp/w3m")
-      (require 'w3m-load)
-      (setq browse-url-browser-function 'w3m-browse-url)))
+;; (if (or (eq system-type 'gnu/linux)
+;; 	(eq system-type 'cyqwin)
+;; 	(eq system-type 'linux))
+;;     (progn
+;;       (add-to-list 'load-path "/usr/share/emacs/site-lisp/w3m")
+;;       (require 'w3m-load)
+;;       (setq browse-url-browser-function 'w3m-browse-url)))
 
