0350edf3a3b6ea9557ac036dba3565cc20b8c510
Peter Christensen <peter@pchristensen.com>
2011-05-19 00:00:04 -0500

    Turns out I do like to have ido directory history

--- emacs.d/pchristensen/ido.el
+++ emacs.d/pchristensen/ido.el
@@ -4,7 +4,7 @@
 ;; http://www.lispcast.com/ido-emacs.html
 (ido-mode t)
 (setq ido-enable-flex-matching t) ; fuzzy matching is a must have
-(setq ido-enable-last-directory-history nil) ; forget latest selected directory names
+(setq ido-enable-last-directory-history t) ; forget latest selected directory names
 
 (autoload 'idomenu "idomenu" nil t)  ; see http://www.emacswiki.org/emacs/ImenuMode#toc10 for contrast
 (global-set-key "\M-i" 'idomenu)
