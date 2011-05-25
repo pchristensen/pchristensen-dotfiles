cae0b521b883f886722b5788d6ebbd25057a3956
Peter Christensen <peter@pchristensen.com>
2011-05-25 11:35:29 -0500

    Changed smex key to use major-mode binding less often

--- emacs.d/pchristensen/smex.el
+++ emacs.d/pchristensen/smex.el
@@ -4,7 +4,7 @@
 (smex-initialize)
 
 (global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "C-x C-m") 'smex-major-mode-commands)
+(global-set-key (kbd "C-x C-m") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 (global-set-key (kbd "C-c C-x x") 'execute-extended-command)
