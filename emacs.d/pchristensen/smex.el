cb205603edb87494390075841ca9c558822530fc
Peter Christensen <peter@pchristensen.com>
2012-04-13 13:44:47 -0500

    Overrode the binding for Mail to smex

--- emacs.d/pchristensen/smex.el
+++ emacs.d/pchristensen/smex.el
@@ -5,6 +5,7 @@
 
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "\C-x m") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 (global-set-key (kbd "C-c C-x x") 'execute-extended-command)
