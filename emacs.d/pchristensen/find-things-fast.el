cfd0b11f690578f0af0182c8a20c81eb6121b070
Peter Christensen <pchristensen@manilla.com>
2012-11-28 16:58:31 -0800

    Configuration (keybindings) for find-things-fast

--- emacs.d/pchristensen/find-things-fast.el
+++ emacs.d/pchristensen/find-things-fast.el
@@ -1,3 +1,10 @@
+;;; find-things-fast
+;;; https://github.com/eglaysher/find-things-fast
+;; Make ftf the default find file, but map ido-find-file as a backup
+(global-set-key "\C-x\C-f" 'ftf-find-file)
+(global-set-key "\C-c\C-f" 'ido-find-file)
+
+(global-set-key "\C-c\C-g" 'ftf-grepsource)
 
 ;;;; TODO Customize by mode for specific file types, e.g.:
 ;; (add-hook 'emacs-lisp-mode-hook
