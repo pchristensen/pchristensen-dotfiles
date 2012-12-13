ac0779e78a6ca8d523d0523c59f369f1c108e906
Peter Christensen <pchristensen@manilla.com>
2012-12-13 12:12:13 -0800

    Clojure mode hooks to auto-start nrepl and switch to it

---
+++ emacs.d/pchristensen/clojure-mode.el
@@ -0,0 +1,10 @@
+;; https://github.com/jochu/clojure-mode
+
+
+(defun force-nrepl-switch-to-repl-buffer ()
+  (interactive)
+  (unless (get-buffer nrepl-connection-buffer)
+    (nrepl-jack-in))
+  (nrepl-switch-to-repl-buffer))
+
+(define-key clojure-mode-map (kbd "C-c C-p") 'force-nrepl-switch-to-repl-buffer)
