920513074b97c2642e75c533b1e46b13527b7e1f
Peter Christensen <pchristensen@manilla.com>
2012-12-19 17:04:23 -0800

    Refactored my clojure-mode to use hooks instead of straight calls

--- emacs.d/pchristensen/clojure-mode.el
+++ emacs.d/pchristensen/clojure-mode.el
@@ -1,11 +1,16 @@
 ;; https://github.com/jochu/clojure-mode
 
+(defun my-clojure-mode-hook ()
+  (rainbow-delimiters))
 
-(defun force-nrepl-switch-to-repl-buffer ()
-  (interactive)
-  (unless (get-buffer nrepl-connection-buffer)
-    (nrepl-jack-in))
-  (nrepl-switch-to-repl-buffer))
-
-(define-key clojure-mode-map (kbd "C-c C-p") 'force-nrepl-switch-to-repl-buffer)
-(define-key clojure-mode-map (kbd "RET") 'paredit-newline)
+(eval-after-load 'clojure-mode
+  '(progn
+     (nrepl-interaction-mode)
+     (define-key clojure-mode-map (kbd "C-c C-p") 'force-nrepl-switch-to-repl-buffer)
+     (define-key clojure-mode-map (kbd "RET") 'paredit-newline)
+     (defun force-nrepl-switch-to-repl-buffer ()
+       (interactive)
+       (unless (get-buffer nrepl-connection-buffer)
+         (nrepl-jack-in))
+       (nrepl-switch-to-repl-buffer))
+     (add-hook 'clojure-mode-hook 'my-clojure-mode-hook)))
