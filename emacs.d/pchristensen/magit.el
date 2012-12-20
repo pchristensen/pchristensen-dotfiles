2064948773de72471c1f088a3a37f365717d95c1
Peter Christensen <pchristensen@manilla.com>
2012-12-19 17:03:31 -0800

    Random cleanup of magit customizations

--- emacs.d/pchristensen/magit.el
+++ emacs.d/pchristensen/magit.el
@@ -3,19 +3,14 @@
 (add-hook
  'magit-mode-hook
  (lambda ()
-   (setq yas/dont-activate t)
-   ))
+   (setq yas/dont-activate t)))
 
 (eval-after-load 'magit
   '(progn
      (set-face-foreground 'magit-diff-add "green3")
      (set-face-foreground 'magit-diff-del "red3")
      (set-face-background 'magit-item-highlight "gray15")
-     ;; (define-key magit-mode-map (kbd "M-3") 'split-window-horizontally) ; was magit-show-level-3
-     ;; (define-key magit-mode-map (kbd "M-2") 'split-window-vertically)   ; was magit-show-level-2
-     ;; (define-key magit-mode-map (kbd "M-1") 'delete-other-windows)      ; was magit-show-level-1
      (global-set-key (kbd "C-x g") 'magit-status)
      (defun magit-pull ()
        (interactive)
-       (magit-run-git-async "pull" "--rebase" "-v"))
-     ))
+       (magit-run-git-async "pull" "--rebase" "-v"))))
