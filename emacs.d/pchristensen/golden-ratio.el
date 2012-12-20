a0acb592d276007b6f5321291dbc743591ef112c
Peter Christensen <pchristensen@manilla.com>
2012-12-20 15:20:12 -0800

    Customized golden ratio to make main window larger

--- emacs.d/pchristensen/golden-ratio.el
+++ emacs.d/pchristensen/golden-ratio.el
@@ -1,4 +1,11 @@
 
 ;; https://github.com/roman/golden-ratio.el
 
+(defadvice -golden-ratio-dimensions (around smaller-ratio)
+  "User a smaller window ratio"
+  (let ((-golden-ratio-value 1.3))
+    ad-do-it))
+
+(ad-activate '-golden-ratio-dimensions)
+
 (golden-ratio-enable)
