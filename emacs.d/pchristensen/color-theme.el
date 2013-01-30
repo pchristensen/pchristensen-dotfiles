7877b71bf1a50aeb2badaf041163186763758928
Peter Christensen <pchristensen@manilla.com>
2013-01-29 16:54:52 -0800

    Fixed color scheme configs

--- emacs.d/pchristensen/color-theme.el
+++ emacs.d/pchristensen/color-theme.el
@@ -1,32 +1,9 @@
 
-;; pretty colors
-(require 'color-theme)
-(setq color-theme-is-global t)
+(load-theme 'solarized-dark t)
+
+;; (require 'color-theme)
+;; (setq color-theme-is-global t)
 
 ;; For osx terminal
-;; (color-theme-euphoria) ;; For puttyterminal
 ;; (color-theme-dark-laptop)
-; (color-theme-tty-dark)
-
-
-;; (vendor 'color-theme-ir-black)
-;; (color-theme-ir-black)
-
-;; ;; Some themese I don't hate
-;; (color-theme-arjen)
-;; (color-theme-simple-1)
-;; (color-theme-calm-forest)
-;; (color-theme-charcoal-black)
-;; (color-theme-gray30)
-;; (color-theme-midnight)
-;; (color-theme-taming-mr-arneson)
 ;; (color-theme-tty-dark)
-;; (color-theme-bharadwaj)
-
-;; ;uncomment if you want to see all themes.  find one you like, and change the color-theme- call above
-;; (require 'theme-manager)
-;; (define-key global-map [f5] 'theme-prev)
-;; (define-key global-map [f6] 'theme-next)
-
-;; (set-face-foreground 'region "white")
-;; (set-face-background 'region "slategray")
