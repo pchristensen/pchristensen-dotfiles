5537c98916287274200c1b046e9d2933a8175e3b
Peter Christensen <pchristensen@manilla.com>
2013-01-30 18:01:47 -0800

    Fix magit colors

--- emacs.d/pchristensen/magit.el
+++ emacs.d/pchristensen/magit.el
@@ -7,9 +7,11 @@
 
 (eval-after-load 'magit
   '(progn
-     (set-face-foreground 'magit-diff-add "green3")
-     (set-face-foreground 'magit-diff-del "red3")
-     (set-face-background 'magit-item-highlight "gray15")
+     (set-face-background 'magit-diff-add "color-234")
+     (set-face-background 'magit-diff-del "color-234")
+     (set-face-background 'magit-diff-none "color-234")
+     (set-face-background 'magit-diff-file-header "color-234")
+     (set-face-background 'magit-diff-hunk-header "color-234")
      (global-set-key (kbd "C-x g") 'magit-status)
      (defun magit-pull ()
        (interactive)
