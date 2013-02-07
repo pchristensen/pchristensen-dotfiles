d87c97176a0d1b6e190cee95e1ce9458364273e3
Peter Christensen <pchristensen@manilla.com>
2013-02-07 14:59:33 -0800

    Added magit-toggle-whitespace

--- emacs.d/pchristensen/magit.el
+++ emacs.d/pchristensen/magit.el
@@ -31,3 +31,24 @@
 
 (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 ;;-----------------------------------------------------------------
+
+;;http://whattheemacsd.com/setup-magit.el-02.html------------------
+(defun magit-toggle-whitespace ()
+  (interactive)
+  (if (member "-w" magit-diff-options)
+      (magit-dont-ignore-whitespace)
+    (magit-ignore-whitespace)))
+
+(defun magit-ignore-whitespace ()
+  (interactive)
+  (add-to-list 'magit-diff-options "-w")
+  (magit-refresh))
+
+(defun magit-dont-ignore-whitespace ()
+  (interactive)
+  (setq magit-diff-options (remove "-w" magit-diff-options))
+  (magit-refresh))
+
+(define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace)
+
+;;-----------------------------------------------------------------
