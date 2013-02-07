a4cb80889f49ca6e3f8639b0adccab7683d4fe08
Peter Christensen <pchristensen@manilla.com>
2013-02-07 14:53:34 -0800

    Magit full screen

--- emacs.d/pchristensen/magit.el
+++ emacs.d/pchristensen/magit.el
@@ -16,3 +16,18 @@
      (defun magit-pull ()
        (interactive)
        (magit-run-git-async "pull" "--rebase" "-v"))))
+
+;; http://whattheemacsd.com/setup-magit.el-01.html-----------------
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+;;-----------------------------------------------------------------
