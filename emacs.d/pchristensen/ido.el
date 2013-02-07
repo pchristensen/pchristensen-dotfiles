fb41246b407b0bc0f8478db7254ee9f5c7222a02
Peter Christensen <pchristensen@manilla.com>
2013-02-07 15:12:01 -0800

    Installed ido-ubiquitous

--- emacs.d/pchristensen/ido.el
+++ emacs.d/pchristensen/ido.el
@@ -8,3 +8,16 @@
 
 (autoload 'idomenu "idomenu" nil t)  ; see http://www.emacswiki.org/emacs/ImenuMode#toc10 for contrast
 (global-set-key "\M-i" 'idomenu)
+
+;;http://whattheemacsd.com/setup-ido.el-01.html--------------------
+;; Use ido everywhere
+(require 'ido-ubiquitous)
+(ido-ubiquitous-mode 1)
+
+;; Fix ido-ubiquitous for newer packages
+(defmacro ido-ubiquitous-use-new-completing-read (cmd package)
+  `(eval-after-load ,package
+     '(defadvice ,cmd (around ido-ubiquitous-new activate)
+        (let ((ido-ubiquitous-enable-compatibility nil))
+          ad-do-it))))
+;;-----------------------------------------------------------------
