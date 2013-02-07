3f77e7b28b9ad69144f85300eae853c965884b88
Peter Christensen <pchristensen@manilla.com>
2013-02-07 15:18:13 -0800

    Toggle window split

--- emacs.d/pchristensen/defuns.el
+++ emacs.d/pchristensen/defuns.el
@@ -114,6 +114,33 @@
      (message "Buffer '%s' is not visiting a file!" name)
    (progn (copy-file filename newname 1) (delete-file filename) (set-visited-file-name newname) (set-buffer-modified-p nil) t))))
 
+;;http://whattheemacsd.com/buffer-defuns.el-03.html-----------------
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+;;-----------------------------------------------------------------
+
 ;;http://whattheemacsd.com/file-defuns.el-01.html------------------
 (defun rename-current-buffer-file ()
   "Renames current buffer and file it is visiting."
