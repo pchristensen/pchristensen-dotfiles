1d82abae87c55f1f3ea40d375933e2d31843ecb3
Peter Christensen <peter@pchristensen.com>
2013-06-11 13:57:57 -0700

    Upgrade emacs packages

---
+++ emacs.d/elpa/nrepl-ritz-20130516.1339/nrepl-ritz-autoloads.el
@@ -0,0 +1,30 @@
+;;; nrepl-ritz-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (nrepl-ritz-jack-in) "nrepl-ritz" "nrepl-ritz.el"
+;;;;;;  (20919 34409))
+;;; Generated autoloads from nrepl-ritz.el
+
+(autoload 'nrepl-ritz-jack-in "nrepl-ritz" "\
+Jack in with a ritz nrepl server.
+If PROMPT-PROJECT is t, then prompt for the project for which to
+start the server.
+
+\(fn &optional PROMPT-PROJECT)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("nrepl-ritz-pkg.el") (20919 34409 52962))
+
+;;;***
+
+(provide 'nrepl-ritz-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; nrepl-ritz-autoloads.el ends here
---
+++ emacs.d/elpa/nrepl-ritz-20130516.1339/nrepl-ritz-pkg.el
@@ -0,0 +1,1 @@
+(define-package "nrepl-ritz" "20130516.1339" "nrepl extensions for ritz [github]" (quote ((nrepl "0.1.7") (fringe-helper "0.1.1"))))
