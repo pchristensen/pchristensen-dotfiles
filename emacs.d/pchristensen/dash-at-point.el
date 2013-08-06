00ec551739d12ee8e90870aec4184265616cd904
Peter Christensen <peter@pchristensen.com>
2013-08-06 13:55:24 -0700

    Add dash-at-point mode for accessing Dash documentation browser

---
+++ emacs.d/pchristensen/dash-at-point.el
@@ -0,0 +1,9 @@
+;; https://github.com/stanaka/dash-at-point#readme
+;; TODO Configure for modes?
+
+(global-set-key (kbd "C-h y") 'dash-at-point)
+
+;; (add-to-list 'dash-at-point-mode-alist '(perl-mode . "perl"))
+
+;; (add-hook 'rinari-minor-mode-hook
+;;           (lambda () (setq dash-at-point-docset "rails")))
