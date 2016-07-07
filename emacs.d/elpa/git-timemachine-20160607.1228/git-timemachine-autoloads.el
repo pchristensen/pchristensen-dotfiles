75fcbd229d44289eeff7eb3f58cbfddd07041ff7
pcchri <pcchri@amazon.com>
2016-07-07 14:11:37 -0700

    Upgrade elisp-slime-nav, git-timemachine, paredit, slime packages

---
+++ emacs.d/elpa/git-timemachine-20160607.1228/git-timemachine-autoloads.el
@@ -0,0 +1,40 @@
+;;; git-timemachine-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (git-timemachine-switch-branch git-timemachine
+;;;;;;  git-timemachine-toggle) "git-timemachine" "git-timemachine.el"
+;;;;;;  (22398 50308 0 0))
+;;; Generated autoloads from git-timemachine.el
+
+(autoload 'git-timemachine-toggle "git-timemachine" "\
+Toggle git timemachine mode.
+
+\(fn)" t nil)
+
+(autoload 'git-timemachine "git-timemachine" "\
+Enable git timemachine for file of current buffer.
+
+\(fn)" t nil)
+
+(autoload 'git-timemachine-switch-branch "git-timemachine" "\
+Enable git timemachine for current buffer, switching to GIT-BRANCH.
+
+\(fn GIT-BRANCH)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("git-timemachine-pkg.el") (22398 50308
+;;;;;;  532842 0))
+
+;;;***
+
+(provide 'git-timemachine-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-timemachine-autoloads.el ends here
---
+++ emacs.d/elpa/git-timemachine-20160607.1228/git-timemachine-pkg.el
@@ -0,0 +1,1 @@
+(define-package "git-timemachine" "20160607.1228" "Walk through git revisions of a file" (quote ((emacs "24.3"))))
